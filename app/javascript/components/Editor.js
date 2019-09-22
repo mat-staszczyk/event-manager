bef2273fc96485a51cb9947c056a3731fc0ebb45
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2019-09-22 20:11:49 +0200

    Introduce React components

---
+++ app/javascript/components/Editor.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header';
+import EventList from './EventList';
+
+function Editor() {
+  const [events, setEvents] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/events.json');
+        setEvents(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (events === null) return null;
+
+  return (
+    <div>
+      <Header />
+      <EventList events={events} />
+    </div>
+  );
+};
+
+export default Editor;
+
