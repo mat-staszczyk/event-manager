7bc70e9537031c582f3d5d944ec3b370585e5e2c
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2019-10-03 18:14:32 +0200

    Add EventForm validation

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -1,49 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { isEmptyObject, validateEvent } from '../helpers/helpers';
 
-function EventForm() {
+function EventForm({ event: initialEvent }) {
+  const [{ event }, setEvent] = useState({ event: initialEvent });
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    setEvent((prevState) => ({
+      event: {
+        ...prevState.event,
+        [name]: value,
+      },
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submitted');
+    const validationErrors = validateEvent(event);
+    if (!isEmptyObject(validationErrors)) {
+      setErrors(validationErrors);
+    } else {
+      console.log('submitted');
+    }
+  };
+
+  const renderErrors = () => {
+    if (isEmptyObject(errors)) {
+      return null;
+    }
+
+    return (
+      <div className="errors">
+        <h3>The following errors prohibited the event from being saved:</h3>
+        <ul>
+          {Object.values(errors).map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      </div>
+    );
   };
 
   return (
     <div>
       <h2>New Event</h2>
+      {renderErrors()}
       <form className="eventForm" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="event_type">
             <strong>Type:</strong>
-            <input type="text" id="event_type" name="event_type" />
+            <input
+              type="text"
+              id="event_type"
+              name="event_type"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="event_date">
             <strong>Date:</strong>
-            <input type="text" id="event_date" name="event_date" />
+            <input
+              type="text"
+              id="event_date"
+              name="event_date"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="title">
             <strong>Title:</strong>
-            <textarea cols="30" rows="10" id="title" name="title" />
+            <textarea
+              cols="30"
+              rows="10"
+              id="title"
+              name="title"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="speaker">
             <strong>Speakers:</strong>
-            <input type="text" id="speaker" name="speaker" />
+            <input
+              type="text"
+              id="speaker"
+              name="speaker"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="host">
             <strong>Hosts:</strong>
-            <input type="text" id="host" name="host" />
+            <input
+              type="text"
+              id="host"
+              name="host"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label htmlFor="published">
             <strong>Publish:</strong>
-            <input type="checkbox" id="published" name="published" />
+            <input
+              type="checkbox"
+              id="published"
+              name="published"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div className="form-actions">
@@ -53,5 +125,20 @@ function EventForm() {
     </div>
   );
 }
+
+EventForm.propTypes = {
+  event: PropTypes.shape(),
+};
+
+EventForm.defaultProps = {
+  event: {
+    event_type: '',
+    event_date: '',
+    title: '',
+    speaker: '',
+    host: '',
+    published: false,
+  },
+};
 
 export default EventForm;
